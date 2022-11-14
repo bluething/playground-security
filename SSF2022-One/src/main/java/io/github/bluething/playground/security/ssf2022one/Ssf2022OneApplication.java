51b294823361847f5cb0a10eb436054eedc67027
Habib Ali Machpud <habib.machpud@gmail.com>
2022-11-14 14:16:53 +0700

    Init ch 1

---
+++ SSF2022-One/src/main/java/io/github/bluething/playground/security/ssf2022one/Ssf2022OneApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.security.ssf2022one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssf2022OneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssf2022OneApplication.class, args);
+	}
+
+}
---
+++ SSF2022-One/src/test/java/io/github/bluething/playground/security/ssf2022one/Ssf2022OneApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.security.ssf2022one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssf2022OneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
